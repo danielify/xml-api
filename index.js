e1865cf196bb2bded750d6b1a54be7c22e945ea7
dbs.toujours <dbs.toujours@gmail.com>
2018-09-27 23:59:03 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 var restify = require('restify');
 
+const object = {
+  nome: "Fátima Teobaldo",
+  observacoes: [{
+    descricao: "Se destacou na atividade: Pintar Coelhos",
+    data: "2018-08-02T03:26:10.063Z"
+  }, {
+    descricao: "Brincou durante a aula",
+    data: "2018-06-28T06:04:11.562Z"
+  }, {
+    descricao: "Brigou durante o intervalo",
+    data: "2018-03-16T02:15:25.473Z"
+  }, {
+    descricao: "Chegou atrazado",
+    data: "2018-04-14T21:49:00.150Z"
+  }],
+  matricula: 437,
+  perfil: [{
+    eixo: "Concentração",
+    turma: 8.2,
+    aluno: 8.4,
+    serie: 3.9
+  }, {
+    eixo: "Cordenação Motora",
+    turma: 9.2,
+    aluno: 9,
+    serie: 9
+  },{
+    eixo: "Dicção",
+    turma: 4,
+    aluno: 4.1,
+    serie: 8.4
+  }, {
+    eixo: "Comunicação",
+    turma: 7.2,
+    aluno: 5.2,
+    serie: 8.2
+  }, {
+    eixo: "Lógica",
+    turma: 8.7,
+    aluno: 5.2,
+    serie: 1.8
+  }]
+}
+
 function respond(req, res, next) {
-  res.send('<?xml version="1.0" encoding="UTF-8" ?><nome>Fátima Teobaldo</nome><observacoes><descricao>Se destacou na atividade: Pintar Coelhos</descricao><data>2018-08-02T03:26:10.063Z</data></observacoes><observacoes><descricao>Brincou durante a aula</descricao><data>2018-06-28T06:04:11.562Z</data></observacoes><observacoes><descricao>Brigou durante o intervalo</descricao><data>2018-03-16T02:15:25.473Z</data></observacoes><observacoes><descricao>Chegou atrazado</descricao><data>2018-04-14T21:49:00.150Z</data></observacoes><matricula>437</matricula><perfil><eixo>Concentração</eixo><turma>8.2</turma><aluno>8.4</aluno><serie>3.9</serie></perfil><perfil><eixo>Cordenação Motora</eixo><turma>9.2</turma><aluno>9</aluno><serie>9</serie></perfil><perfil><eixo>Dicção</eixo><turma>4</turma><aluno>4.1</aluno><serie>8.4</serie></perfil><perfil><eixo>Comunicação</eixo><turma>7.2</turma><aluno>5.2</aluno><serie>8.2</serie></perfil><perfil><eixo>Lógica</eixo><turma>8.7</turma><aluno>5.2</aluno><serie>1.8</serie></perfil>');
+  res.end(js2xmlparser.parse("person", object));
   next();
 }
 
